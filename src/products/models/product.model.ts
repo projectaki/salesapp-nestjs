de1fc251898b87a292d384d1025ca6faf9be2c30
Akos <akosegypro@gmail.com>
2021-10-03 22:42:23 +0200

    add :heavy_plus_sign: typeorm entity fields to product model

--- src/products/models/product.model.ts
+++ src/products/models/product.model.ts
@@ -1,13 +1,38 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Product {
   @Field()
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Field()
+  @Column({ length: 500, nullable: false })
   name: string;
 
   @Field((type) => Int)
+  @Column()
   price: number;
+
+  @Field()
+  @Column()
+  @CreateDateColumn()
+  created_at: Date;
+
+  @Field()
+  @Column()
+  @UpdateDateColumn()
+  updated_at: Date;
 }
+
+// @Field(type => [InvoiceModel], { nullable: true })
+// @OneToMany(type => InvoiceModel, invoice => invoice.customer)
+// invoices: InvoiceModel[]
