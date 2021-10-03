c5b0871ec925124e2db63710d87769180533f0b0
Akos <akosegypro@gmail.com>
2021-10-03 22:43:04 +0200

    add :heavy_plus_sign: mutation/createProduct to resolver

--- src/products/products.resolver.ts
+++ src/products/products.resolver.ts
@@ -1,15 +1,31 @@
-import { Args, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { Product } from './models/product.model';
+import { ProductService } from './services/product.service';
 
 // In the code first method, a resolver class both defines resolver functions and generates the Query type.
 @Resolver((of) => Product)
 export class ProductsResolver {
-  // constructor() {}
+  constructor(private productService: ProductService) {}
 
   @Query((returns) => Product, { name: 'product' }) // param => supply a parent object used by field resolver functions as they traverse down through an object graph
   async getProduct(@Args('id') id: string) {
     // args can be called multiple times, and seperated into a seperate file to avoid bloating
     return { id, name: 'TV', price: 5000 } as Product;
+  }
+
+  @Mutation((returns) => Product)
+  async createProduct(
+    @Args('name') name: string,
+    @Args('price') price: number,
+  ): Promise<Product> {
+    return await this.productService.create({ name, price });
   }
 
   //   @ResolveField('children', (returns) => [{ name: string, productId: number }])
