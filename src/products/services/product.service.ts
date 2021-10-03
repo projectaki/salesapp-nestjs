f709430f82824b357844b2cf058d3aee731f3398
Akos <akosegypro@gmail.com>
2021-10-03 22:43:20 +0200

    add :new: product service

---
+++ src/products/services/product.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from '../models/product.model';
+
+@Injectable()
+export class ProductService {
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ) {}
+
+  create = (product: any): Promise<Product> => {
+    return this.productRepository.save(product);
+  };
+
+  findOne(id: string): Promise<Product> {
+    return this.productRepository.findOne(id);
+  }
+}
+
+// Below is how to make a transaction
+// constructor(private connection: Connection) {}
+// async createMany(users: User[]) {
+//   const queryRunner = this.connection.createQueryRunner();
+
+//   await queryRunner.connect();
+//   await queryRunner.startTransaction();
+//   try {
+//     await queryRunner.manager.save(users[0]);
+//     await queryRunner.manager.save(users[1]);
+
+//     await queryRunner.commitTransaction();
+//   } catch (err) {
+//     // since we have errors lets rollback the changes we made
+//     await queryRunner.rollbackTransaction();
+//   } finally {
+//     // you need to release a queryRunner which was manually instantiated
+//     await queryRunner.release();
+//   }
+// }
