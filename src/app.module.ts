9d02abf62756266084752c8e52a03ba7ab5817a5
Akos <akosegypro@gmail.com>
2021-10-09 02:16:34 +0200

    :new: Queue system

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,21 +8,31 @@ import { ScrapersModule } from './scrapers/scrapers.module';
 import { SharedModule } from './shared/shared.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TaskSchedulerModule } from './task-scheduler/task-scheduler.module';
+import { BullModule } from '@nestjs/bull';
+import { QueueProcessorService } from './queue-processor/queue-processor.service';
+import { join } from 'path';
+import { QueueProcessorModule } from './queue-processor/queue-processor.module';
 
 @Module({
   imports: [
     ProductsModule,
     ScrapersModule,
+    TaskSchedulerModule,
     GraphQLModule.forRoot({
       // debug: false,
       // playground: false,
       autoSchemaFile: true, //In the code first approach, you use decorators and TypeScript classes to generate the corresponding GraphQL schema.
       //autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
     TypeOrmModule.forRoot(),
     ScheduleModule.forRoot(),
     SharedModule,
-    TaskSchedulerModule,
   ],
   controllers: [AppController],
   providers: [
