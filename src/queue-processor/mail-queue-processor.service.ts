495719c6fbf79bac7efb29f1aa76c8f6ed0544b3
Akos <akosegypro@gmail.com>
2021-10-10 20:09:17 +0200

    :new: Add Mail queue

---
+++ src/queue-processor/mail-queue-processor.service.ts
@@ -0,0 +1,15 @@
+import { Process, Processor } from '@nestjs/bull';
+import { Job } from 'bull';
+import { MailService } from 'src/mail/mail.service';
+import { Product } from 'src/products/models/product.model';
+
+@Processor('mail')
+export class MailQueueProcessorService {
+  constructor(private readonly mail: MailService) {}
+  @Process('email')
+  async processProduct(job: Job<Product[]>) {
+    console.log('Processing mail');
+    this.mail.sendEmail(JSON.stringify(job.data));
+    return {};
+  }
+}
--- src/queue-processor/queue-processor.module.ts
+++ src/queue-processor/queue-processor.module.ts
@@ -4,16 +4,22 @@ import { MailModule } from 'src/mail/mail.module';
 import { ProductsModule } from 'src/products/products.module';
 import { ProductService } from 'src/products/services/product.service';
 import { QueueProcessorService } from './queue-processor.service';
+import { MailQueueProcessorService } from './mail-queue-processor.service';
 
 @Module({
   imports: [
-    BullModule.registerQueue({
-      name: 'product-queue',
-    }),
+    BullModule.registerQueue(
+      {
+        name: 'product-queue',
+      },
+      {
+        name: 'mail',
+      },
+    ),
     ProductsModule,
     MailModule,
   ],
-  providers: [QueueProcessorService],
+  providers: [QueueProcessorService, MailQueueProcessorService],
   exports: [QueueProcessorService, BullModule],
 })
 export class QueueProcessorModule {}
