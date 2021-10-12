7e24bd5f9e5c161d8a3257cfa2b64d7e80e6041c
Akos <akosegypro@gmail.com>
2021-10-12 21:22:44 +0200

    :recycle: logger to log depending if environment is development or not

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -4,6 +4,18 @@ import {
   LoggerService,
   LogLevel,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class LoggingService extends ConsoleLogger {}
+export class LoggingService extends ConsoleLogger {
+  isDevelopment: boolean;
+  constructor(private configService: ConfigService) {
+    super();
+    this.isDevelopment = this.configService.get<boolean>('IS_DEVELOPMENT');
+  }
+
+  Log(message: string, ...args: any) {
+    if (this.isDevelopment) super.log(message, ...args);
+    else console.log(message, ...args);
+  }
+}
