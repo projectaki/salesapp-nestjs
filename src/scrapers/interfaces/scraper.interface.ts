d89922286ccb2ea07f510c082d669737711bd1f5
Akos <65110142+projectaki@users.noreply.github.com>
2021-10-16 15:45:23 +0200

    :recycle: Refactor scraper to be more testable (#16)

--- src/scrapers/interfaces/scraper.interface.ts
+++ src/scrapers/interfaces/scraper.interface.ts
@@ -1,5 +1,14 @@
+import { Page } from 'puppeteer';
 import { Product } from 'src/products/models/product.model';
 
 export interface IScraper {
-  getProducts(): Promise<Product[]>;
+  getAllProducts(): Promise<Product[]>;
+  _getUniqueDiscountedProducts(page: Page): Promise<Product[]>;
+  _loadHtmlFromUrl(page: Page, url: string);
+  _getProductsFromHtml(html: string): Product[];
+  _getPageNumberOrDefault(
+    page: Page,
+    url: string,
+    maxPages?: number,
+  ): Promise<number>;
 }
