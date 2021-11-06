e56919ac2c0b3d1b46b6d76ce1c99ed4dd339516
Akos <akosegypro@gmail.com>
2021-11-06 14:19:17 +0100

    :x: Start removing redis queue and typeorm/postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
-import { BullModule } from '@nestjs/bull';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule } from './core/logger/logger.module';
 import { UserModule } from './modules/users/user.module';
@@ -18,17 +16,6 @@ import { AuthModule } from './core/auth/auth.module';
       autoSchemaFile: true, //In the code first approach, you use decorators and TypeScript classes to generate the corresponding GraphQL schema.
       //autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
-    BullModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        redis: {
-          host: configService.get('REDIS_HOST'),
-          port: +configService.get('REDIS_PORT'),
-        },
-      }),
-      inject: [ConfigService],
-    }),
-    TypeOrmModule.forRoot(),
     ScheduleModule.forRoot(),
     ConfigModule.forRoot({ isGlobal: true }),
     LoggerModule,
